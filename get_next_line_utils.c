de9d23c641a33de2934be90e90a096bf94902bc9
Uliana Nadorozhna <unadoroz@c2r4p4.hive.fi>
2025-05-13 10:30:23 +0300

    created the structure for the project

---
+++ get_next_line_utils.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: unadoroz <unadoroz@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 09:15:42 by unadoroz          #+#    #+#             */
+/*   Updated: 2025/05/13 10:06:09 by unadoroz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*result;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	result = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!result)
+		return (NULL);
+	while (s1[i])
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		result[i] = s2[j];
+		i++;
+		j++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*new;
+	int		i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	new = (char *)malloc(sizeof(char) * (i + 1));
+	if (new == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		new[i] = s[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+i}
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned char	ch;
+	int				i;
+
+	i = 0;
+	ch = (unsigned char)c;
+	while (s[i])
+	{
+		if (s[i] == ch)
+			return ((char *)(s + i));
+		i++;
+	}
+	if (ch == '\0')
+		return ((char *)(s + i));
+	return (NULL);
+}
